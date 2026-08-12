0ced8abe4d19b273a632981fafb5eb8ab084e468
fadila-strawhat <fadilahibrahim49@gmail.com>
2026-08-12 16:55:55 +0100

    update all

--- script.js
+++ script.js
@@ -3,3 +3,178 @@ function contact(){
 }
 
 //this is a comment//
+/*
+name = "Mercy"
+age = 19
+country = "Nigeria"
+favourite_colour = "Pink"
+favourite_food = "Jollof Rice"
+
+console.log("Name: " + name)
+console.log("Age: " + age)
+console.log("Country: " + country)
+console.log("Favourite Colour: " + favourite_colour)
+console.log("Favourite Food: " + favourite_food)
+
+
+name = prompt("Enter your name")
+document.write(name)
+
+num1 = Number(prompt("Enter your number ?"))
+num2 = Number(prompt("Enter 2nd number ?"))
+document.write(num1 + num2)
+
+
+name = prompt("Enter your name")
+document.write("<h1>Welcome, " + name + "!</h1>")
+
+_name = prompt("Enter your name")
+_age = prompt("Enter your age")
+document.write("Welcome, " + _name + " you are " + _age + "years old")
+console.log(`Welcome,${_name} you are ${_age} years old`)
+confirm("Are you sure you want to continue?")
+
+
+
+let name = prompt("Enter your name")
+let favcolor = prompt("Enter your favourite colour")
+document.write(`My favourite colour is ${favcolor}`)
+document.write(`My name is ${name}`)
+
+
+
+let  age = prompt("Enter ur age");
+if (age>=18){
+    alert("You are old enough to watch the movie")
+}
+else{
+    alert("U are not old enough to watch the movie")
+}
+    
+
+let food = prompt("Enter your favourite food")
+  if (food === "Jollof Rice"){
+      alert("You are a Rice lover")
+  }
+  else{
+    alert("You are not a Rice lover")
+  }
+
+let score = prompt("Enter your score")
+if (score >= 70){
+    alert("You got an A")
+}
+else if (score >= 50){
+    alert("You got a B")
+}
+
+else if (score >= 30){
+  alert("You got a C")
+}
+
+else{
+    alert("You got a D")
+}
+    
+
+
+let day = prompt("Enter the day")
+switch (day){
+    case "Monday":
+   alert("It is Monday🤬")
+   break;
+
+   case"Friday":
+   alert("It is Finally Friday😎")
+   break;
+
+   case"Saturday":
+   alert("It is the glorious Saturday😉")
+   break;
+
+   case"Sunday":
+   alert("The weekend is over😔")
+   break;
+   default:
+    alert("It is a weekday😭")
+    break;
+
+}
+   
+
+for(let i = 0; i <= 100; i++){
+    console.log(i);
+}
+ 
+let number = Number(prompt("Enter a number"));
+
+for(let i = 1; i <= 12; i++){
+
+    console.log(`${number} × ${i} = ${number * i}`);
+}
+
+
+
+let i = 1;
+
+while(i <= 10){
+    console.log(i);
+
+    i++;
+}
+
+function sayHello(){
+    alert("Hello World")
+}
+
+sayHello()
+
+
+function add(num1, num2){
+
+    return num1 + num2;
+
+}
+
+let answer = add(10, 5);
+
+console.log(answer);
+
+
+
+
+function greet(name,age,school){
+console.log(`Hello ${name} you are ${age} years old and u are currently attending ${school}`);
+
+}
+ greet("Dila",20,"pinkspring")
+
+
+ function add(num1, num2){
+
+    return num1 + num2;
+
+}
+
+let answer = add(10, 5);
+
+console.log(answer);
+function test(){
+
+function test(){
+
+    let name = "Dila";
+
+    console.log(name);
+
+}
+
+test();
+*/
+
+let student = {
+    name: "Mercy",
+    age: 21,
+    hobby:,
+
+}
